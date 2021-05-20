5bd54a311fd0a15ec8617ab8cc9c8300613120ee
Kasimashi <Kasimashi@hotmail.com>
2021-05-20 15:01:18 +0200

    Mise à jour scrupt ECB

--- Crypto/AES_ECB/AES_ECB.py
+++ Crypto/AES_ECB/AES_ECB.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
+import binascii
 from Crypto.Cipher import AES
  
-key = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
- 
-cipher = AES.new(key, AES.MODE_ECB)
-msg =cipher.encrypt('TOTOTOTOTOTOTOTO')
-print (type(msg))
- 
-print(msg.hex())
- 
+key = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+buff = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
+print("Key : ")
+print(binascii.hexlify(key))
+
+print("Buffer : ")
+print(binascii.hexlify(buff))
+
+cipher = AES.new(key, AES.MODE_ECB)
+print("Encrypt:")
+print(cipher.encrypt(buff).hex())
+ 
+print("Decrypt:")
 decipher = AES.new(key, AES.MODE_ECB)
-print(decipher.decrypt(msg))
+print(decipher.decrypt(buff).hex())
